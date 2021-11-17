ccc4a765211d57917c8b51666e4f728a92750e1a
Bryan <bryanmartz972@gmail.com>
2021-11-17 12:07:54 -0600

    Boton de filtrar

--- src/componentes/principal.js
+++ src/componentes/principal.js
@@ -14,9 +14,14 @@ export default function App({ navigation }) {
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
             <Text style={styles.tituloPrometheus}>PROMETHEUS</Text>
+            <View style={styles.headerbotones}>
+            <Pressable>
+                <Image source={require('../../assets/img/filtrar.png')}/>
+            </Pressable>
             <Pressable onPress={() => navigation.replace('Login')}>
                 <Image source={require('../../assets/img/exit.png')}/>
             </Pressable>
+            </View>
           </View>
             <ScrollView style={styles.main}>
               <Image source={require('../../assets/img/adidas1.jpg')} styles={styles.imagen}/>
@@ -81,41 +86,11 @@ const styles = StyleSheet.create({
       fontSize: 24,
       fontFamily: 'montserrat-bold'
     },
-    entradaTexto: {
-      backgroundColor: '#fff',
-      fontFamily: 'montserrat-semibold',
-      height: 32,
-      marginTop: 10,
-      marginBottom: 10
-    },
-    contenedorBotones: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    },
-    botones: {
-      marginTop: 10,
-      paddingTop: 5,
-      paddingBottom: 5,
-      paddingLeft: 30,
-      paddingRight: 30,
-      borderRadius: 5,
-      backgroundColor: '#ed7731',
-    },
-    tituloBotones: {
-    color: "#072C50",
-    fontSize: 26,
-    fontWeight: "700",
-    },
-    productosTexto: {
-      color: '#ed7731',
-      fontSize: 18,
-    },
-    contenedorInfo: {
-      marginRight: 10,
-      marginBottom: 20,
-    },
     imagen: {
       marginTop: 20,
+    },
+    headerbotones: {
+      display: 'flex',
+      flexDirection: 'row',
     }
 });
